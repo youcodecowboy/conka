5ceb358dab560d2e0a11a3742d85c5b2b2cba470
Rudh <rudhkurup@conka.uk>
2026-01-09 15:44:26 +0000

    win prize and reassurance

--- app/components/win/index.ts
+++ app/components/win/index.ts
@@ -2,3 +2,5 @@ export { default as WinHero } from "./WinHero";
 export { default as WinCountdown } from "./WinCountdown";
 export { default as WinEmailForm } from "./WinEmailForm";
 export { default as WinThankYou } from "./WinThankYou";
+export { default as WinPrize } from "./WinPrize";
+export { default as WinReassurance } from "./WinReassurance";
