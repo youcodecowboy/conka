9453b6a3a9e89475eb4abf0fef5dd9824c976a9c
Rudh <rudhkurup@conka.uk>
2026-01-09 16:57:35 +0000

    adding pages to index

--- app/components/win/index.ts
+++ app/components/win/index.ts
@@ -4,3 +4,5 @@ export { default as WinEmailForm } from "./WinEmailForm";
 export { default as WinThankYou } from "./WinThankYou";
 export { default as WinPrize } from "./WinPrize";
 export { default as WinReassurance } from "./WinReassurance";
+export { default as WinPageDesktop } from "./WinPageDesktop";
+export { default as WinPageMobile } from "./WinPageMobile";
