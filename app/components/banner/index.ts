a8721bca7b38f7456cff3a439cc4e45ddda4952f
Rudh <rudhkurup@conka.uk>
2026-01-12 17:42:44 +0000

    updating index

--- app/components/banner/index.ts
+++ app/components/banner/index.ts
@@ -1,4 +1,12 @@
 // Banner feature exports
-export type { BannerConfig, BannerVariant, BannerButton, BannerContent, BannerStyling } from "./types";
+export type {
+  BannerConfig,
+  BannerVariant,
+  BannerButton,
+  BannerContent,
+  BannerStyling,
+} from "./types";
 export { default as Banner } from "./Banner";
+export { default as BannerDesktop } from "./BannerDesktop";
+export { default as BannerMobile } from "./BannerMobile";
 export { useBannerConfig, foundingMemberConfig } from "./bannerConfig";
