6c4c60d3767843b0dac00c9fe570e0542863d486
Rudh <rudhkurup@conka.uk>
2026-01-12 18:46:15 +0000

    adding founding member counter

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,6 +8,7 @@
  * @module
  */
 
+import type * as foundingMemberCounter from "../foundingMemberCounter.js";
 import type * as quizAnalytics from "../quizAnalytics.js";
 import type * as winEntries from "../winEntries.js";
 
@@ -18,6 +19,7 @@ import type {
 } from "convex/server";
 
 declare const fullApi: ApiFromModules<{
+  foundingMemberCounter: typeof foundingMemberCounter;
   quizAnalytics: typeof quizAnalytics;
   winEntries: typeof winEntries;
 }>;
