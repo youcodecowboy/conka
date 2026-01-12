03ed5469860be222ac188c7a746d639f8db060d5
Rudh <rudhkurup@conka.uk>
2026-01-12 17:38:00 +0000

    index of components

---
+++ app/components/banner/index.ts
@@ -0,0 +1,4 @@
+// Banner feature exports
+export type { BannerConfig, BannerVariant, BannerButton, BannerContent, BannerStyling } from "./types";
+export { default as Banner } from "./Banner";
+export { useBannerConfig, foundingMemberConfig } from "./bannerConfig";
