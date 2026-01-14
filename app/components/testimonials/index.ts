6f36e6bce56b5a4d9c4ae80f597b72d756b7b42a
Rudh <rudhkurup@conka.uk>
2026-01-14 12:50:20 +0000

    adding new comopnet to index

--- app/components/testimonials/index.ts
+++ app/components/testimonials/index.ts
@@ -3,10 +3,15 @@
  */
 
 // Types first
-export type { Testimonial, TestimonialsProps, TestimonialCardProps } from "./types";
+export type {
+  Testimonial,
+  TestimonialsProps,
+  TestimonialCardProps,
+} from "./types";
 
 // Components
 export { default as Testimonials } from "./Testimonials";
 export { default as TestimonialsDesktop } from "./TestimonialsDesktop";
 export { default as TestimonialsMobile } from "./TestimonialsMobile";
 export { default as TestimonialCard } from "./TestimonialCard";
+export { default as ScrollIndicator } from "./ScrollIndicator";
