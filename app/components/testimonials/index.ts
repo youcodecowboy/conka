f23153a68d292d1b932ce77b4cba7b3bd662c9f5
Rudh <rudhkurup@conka.uk>
2026-01-14 12:19:05 +0000

    index of components

---
+++ app/components/testimonials/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Barrel exports for testimonials components
+ */
+
+// Types first
+export type { Testimonial, TestimonialsProps, TestimonialCardProps } from "./types";
+
+// Components
+export { default as Testimonials } from "./Testimonials";
+export { default as TestimonialsDesktop } from "./TestimonialsDesktop";
+export { default as TestimonialsMobile } from "./TestimonialsMobile";
+export { default as TestimonialCard } from "./TestimonialCard";
