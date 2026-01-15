0bf5bdc1f61f9101926a16ea80c33ce9997cfb1b
Rudh <rudhkurup@conka.uk>
2026-01-15 13:05:21 +0000

    adding two files to index

--- app/components/shop/index.ts
+++ app/components/shop/index.ts
@@ -1,5 +1,7 @@
 // Shop components barrel export
 export { default as ShopHero } from "./ShopHero";
+export { default as ShopHeroDesktop } from "./ShopHeroDesktop";
+export { default as ShopHeroMobile } from "./ShopHeroMobile";
 export { default as ProtocolsGrid } from "./ProtocolsGrid";
 export { default as FormulasShowcase } from "./FormulasShowcase";
 export { default as TrialPacksShop } from "./TrialPacksShop";
