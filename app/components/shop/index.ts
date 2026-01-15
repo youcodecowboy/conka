7ab5b68e81bc88fb806c7c8b6416a1d9bcdf2a7b
Rudh <rudhkurup@conka.uk>
2026-01-15 14:07:57 +0000

    adding to index

--- app/components/shop/index.ts
+++ app/components/shop/index.ts
@@ -3,5 +3,7 @@ export { default as ShopHero } from "./ShopHero";
 export { default as ShopHeroDesktop } from "./ShopHeroDesktop";
 export { default as ShopHeroMobile } from "./ShopHeroMobile";
 export { default as ProtocolsGrid } from "./ProtocolsGrid";
+export { default as ProtocolsGridDesktop } from "./ProtocolsGridDesktop";
+export { default as ProtocolsGridMobile } from "./ProtocolsGridMobile";
 export { default as FormulasShowcase } from "./FormulasShowcase";
 export { default as TrialPacksShop } from "./TrialPacksShop";
