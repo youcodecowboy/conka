d9c3a8f11a9f7c16ee6edd05eebe1c6d8eb7115f
Rudh <rudhkurup@conka.uk>
2026-01-15 17:29:37 +0000

    updating index with new files

--- app/components/shop/index.ts
+++ app/components/shop/index.ts
@@ -5,4 +5,10 @@ export { default as ShopHeroMobile } from "./ShopHeroMobile";
 export { default as ProtocolsGrid } from "./ProtocolsGrid";
 export { default as ProtocolsGridDesktop } from "./ProtocolsGridDesktop";
 export { default as ProtocolsGridMobile } from "./ProtocolsGridMobile";
+export { default as ProtocolSelector } from "./ProtocolSelector";
+export { default as ProtocolActivePanel } from "./ProtocolActivePanel";
+export { default as ProtocolRatioWheel } from "./ProtocolRatioWheel";
 export { default as FormulasShowcase } from "./FormulasShowcase";
+
+// Data exports
+export * from "./protocolSelectorData";
