9b9fd134780907f978359d6b495990a71d853f0f
Rudh <rudhkurup@conka.uk>
2026-01-17 11:27:23 +0000

    removing from index

--- app/components/shop/index.ts
+++ app/components/shop/index.ts
@@ -6,7 +6,6 @@ export { default as ProtocolsGrid } from "./ProtocolsGrid";
 export { default as ProtocolsGridDesktop } from "./ProtocolsGridDesktop";
 export { default as ProtocolsGridMobile } from "./ProtocolsGridMobile";
 export { default as ProtocolCardMobile } from "./ProtocolCardMobile";
-export { default as ProtocolActivePanel } from "./ProtocolActivePanel";
 export { default as FormulasShowcase } from "./FormulasShowcase";
 export { default as FormulasShowcaseDesktop } from "./FormulasShowcaseDesktop";
 export { default as FormulasShowcaseMobile } from "./FormulasShowcaseMobile";
