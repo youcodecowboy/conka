ce7d91e917e28202e128106e32f6a872b6c17736
Rudh <rudhkurup@conka.uk>
2026-01-22 14:44:11 +0000

    adding account icon in header that takes us to account login

--- app/components/navigation/NavigationDesktop.tsx
+++ app/components/navigation/NavigationDesktop.tsx
@@ -185,6 +185,28 @@ export default function NavigationDesktop({
                   hideBanner={hideBanner}
                 />
               </div>
+
+              {/* Account Icon */}
+              <a
+                href="/account/login"
+                className="p-2 hover:opacity-70 transition-all"
+                aria-label="Account"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" />
+                  <circle cx="12" cy="7" r="4" />
+                </svg>
+              </a>
             </nav>
             <button
               onClick={openCart}
