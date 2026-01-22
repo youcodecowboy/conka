de4fc0d7ff91360d7368d9441886c014f74f7926
Rudh <rudhkurup@conka.uk>
2026-01-22 19:04:15 +0000

    app store icon used

--- app/components/AppInstallButtons.tsx
+++ app/components/AppInstallButtons.tsx
@@ -10,7 +10,7 @@ interface AppInstallButtonsProps {
 const APP_STORE_URL = "https://apps.apple.com/gb/app/conka-app/id6450399391";
 const PLAY_STORE_URL = "https://play.google.com/store/apps/details?id=com.conka.conkaApp&hl=en_GB";
 
-// Official App Store icon (Apple logo)
+// Official App Store icon (rounded square with stylized "A")
 const AppStoreIcon = ({ size }: { size: number }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -19,7 +19,7 @@ const AppStoreIcon = ({ size }: { size: number }) => (
     viewBox="0 0 24 24"
     fill="currentColor"
   >
-    <path d="M17.05 20.28c-.98.95-2.05.88-3.08.4-1.09-.5-2.08-.48-3.24 0-1.44.62-2.2.44-3.06-.4C2.79 15.25 3.51 7.59 9.05 7.31c1.35.07 2.29.74 3.08.8 1.18-.24 2.31-.93 3.57-.84 1.56.15 2.92.93 3.86 2.26-3.23 1.94-2.7 5.99.51 7.31-.02 1.99.45 4.05-1.01 5.65zm-3.95-18.4c.15-1.9 1.73-3.38 3.74-3.57.3 2.37-1.95 4.4-3.74 3.57z" />
+    <path d="M18.71 19.5c-.83 1.24-1.71 2.45-3.05 2.47-1.34.03-1.77-.79-3.29-.79-1.53 0-2 .77-3.27.82-1.31.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.39c.87-1.52 2.43-2.48 4.12-2.51 1.28-.02 2.5.87 3.29.87.78 0 2.26-1.07 3.81-.91.65.03 2.47.26 3.64 1.98-.09.06-2.17 1.28-2.15 3.81.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M13 3.5c.73-.83 1.94-1.46 2.94-1.5.13 1.17-.34 2.35-1.04 3.19-.69.85-1.83 1.51-2.95 1.42-.15-1.15.41-2.35 1.05-3.11z" />
   </svg>
 );
 
