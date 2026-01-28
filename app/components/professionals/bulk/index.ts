f15770c4468120c96e4b373b60209f6d365d6bef
Rudh <rudhkurup@conka.uk>
2026-01-28 14:10:10 +0000

    benefit section

--- app/components/professionals/bulk/index.ts
+++ app/components/professionals/bulk/index.ts
@@ -4,4 +4,9 @@ export type { BulkFormulaCardProps } from "./types";
 // Components
 export { default as BulkPurchaseHeader } from "./BulkPurchaseHeader";
 export { default as BulkFormulaCard } from "./BulkFormulaCard";
-export { default as FormulaInfoSections } from "./FormulaInfoSections";
+export {
+  InfoSection,
+  BenefitsSection,
+  IngredientsSection,
+  TasteSection,
+} from "./FormulaInfoSections";
