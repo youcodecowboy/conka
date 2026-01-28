39260085860b198b38c46b3fe0506d27f8efd532
Rudh <rudhkurup@conka.uk>
2026-01-28 12:01:50 +0000

    individual purchase page

---
+++ app/components/professionals/individual/IndividualPurchasePage.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import ProtocolsSection from "./ProtocolsSection";
+import FormulasSection from "./FormulasSection";
+
+export default function IndividualPurchasePage() {
+  return (
+    <>
+      <ProtocolsSection />
+      <FormulasSection />
+    </>
+  );
+}
