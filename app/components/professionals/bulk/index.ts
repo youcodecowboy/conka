fcbd35d7c2c32eca7679b173f1dc1725d3c736a6
Rudh <rudhkurup@conka.uk>
2026-01-28 14:04:39 +0000

    add to index

--- app/components/professionals/bulk/index.ts
+++ app/components/professionals/bulk/index.ts
@@ -4,3 +4,4 @@ export type { BulkFormulaCardProps } from "./types";
 // Components
 export { default as BulkPurchaseHeader } from "./BulkPurchaseHeader";
 export { default as BulkFormulaCard } from "./BulkFormulaCard";
+export { default as FormulaInfoSections } from "./FormulaInfoSections";
