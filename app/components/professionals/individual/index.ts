f6593503f89f7df0db4d70946e8b1caca8d7721a
Rudh <rudhkurup@conka.uk>
2026-01-28 12:02:02 +0000

    index

---
+++ app/components/professionals/individual/index.ts
@@ -0,0 +1,12 @@
+// Types
+export type {
+  ProtocolPurchaseCardProps,
+  FormulaPurchaseCardProps,
+} from "./types";
+
+// Components
+export { default as IndividualPurchasePage } from "./IndividualPurchasePage";
+export { default as ProtocolsSection } from "./ProtocolsSection";
+export { default as FormulasSection } from "./FormulasSection";
+export { default as ProtocolPurchaseCard } from "./ProtocolPurchaseCard";
+export { default as FormulaPurchaseCard } from "./FormulaPurchaseCard";
