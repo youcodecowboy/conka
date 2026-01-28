627a723f8ae2c5c2c659601b5e95310a249de187
Rudh <rudhkurup@conka.uk>
2026-01-28 12:02:05 +0000

    types

---
+++ app/components/professionals/individual/types.ts
@@ -0,0 +1,15 @@
+import { ProtocolId, FormulaId } from "@/app/lib/productData";
+
+/**
+ * Props for ProtocolPurchaseCard component
+ */
+export interface ProtocolPurchaseCardProps {
+  protocolId: ProtocolId;
+}
+
+/**
+ * Props for FormulaPurchaseCard component
+ */
+export interface FormulaPurchaseCardProps {
+  formulaId: FormulaId;
+}
