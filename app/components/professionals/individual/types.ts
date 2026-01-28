4c5d75fb00b100605dd89c7e580803415fdf13d5
Rudh <rudhkurup@conka.uk>
2026-01-28 12:08:03 +0000

    updating types to have protocol selecor

--- app/components/professionals/individual/types.ts
+++ app/components/professionals/individual/types.ts
@@ -1,10 +1,28 @@
-import { ProtocolId, FormulaId } from "@/app/lib/productData";
+import {
+  ProtocolId,
+  FormulaId,
+  ProtocolTier,
+  PurchaseType,
+} from "@/app/lib/productData";
 
 /**
- * Props for ProtocolPurchaseCard component
+ * Props for ProtocolSelector component
  */
-export interface ProtocolPurchaseCardProps {
+export interface ProtocolSelectorProps {
+  selectedProtocol: ProtocolId | null;
+  onSelect: (protocolId: ProtocolId) => void;
+}
+
+/**
+ * Props for ExpandedProtocolView component
+ */
+export interface ExpandedProtocolViewProps {
   protocolId: ProtocolId;
+  selectedTier: ProtocolTier;
+  onTierSelect: (tier: ProtocolTier) => void;
+  purchaseType: PurchaseType;
+  onPurchaseTypeChange: (type: PurchaseType) => void;
+  onAddToCart: () => void;
 }
 
 /**
