71e2c871eb8bda24329a49992dd9f9d2ddaa253e
Rudh <rudhkurup@conka.uk>
2026-02-11 15:31:40 +0000

    using red

--- app/components/navigation/protocolImageConfig.ts
+++ app/components/navigation/protocolImageConfig.ts
@@ -6,7 +6,7 @@
 
 // Protocol navigation images (for cards, menus)
 export const protocolNavigationImages: Record<string, string> = {
-  "1": "/protocols/ResilienceBox.jpg",
+  "1": "/protocols/ResilienceRed.jpg",
   "2": "/protocols/PrecisionPurple.jpg",
   "3": "/protocols/BalanceGreen.jpg",
   "4": "/protocols/UltimatePink.jpg",
