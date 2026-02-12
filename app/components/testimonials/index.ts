2c5806b124ed0702ce2d0bd3ec5e23e0b279d338
Rudh <rudhkurup@conka.uk>
2026-02-12 12:24:31 +0000

    delete legacy testimonial comopnent

--- app/components/testimonials/index.ts
+++ app/components/testimonials/index.ts
@@ -12,7 +12,5 @@ export type {
 
 // Components
 export { default as Testimonials } from "./Testimonials";
-export { default as TestimonialsDesktop } from "./TestimonialsDesktop";
-export { default as TestimonialsMobile } from "./TestimonialsMobile";
 export { default as TestimonialCard } from "./TestimonialCard";
 export { default as ScrollIndicator } from "./ScrollIndicator";
