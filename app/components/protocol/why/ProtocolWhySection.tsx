047f925769ba433a93fc9f12bb470d8bdcac274f
Rudh <rudhkurup@conka.uk>
2026-02-12 11:56:00 +0000

    protocol id for accent

--- app/components/protocol/why/ProtocolWhySection.tsx
+++ app/components/protocol/why/ProtocolWhySection.tsx
@@ -31,7 +31,7 @@ export default function ProtocolWhySection({
       />
 
       <CycleBreak />
-      <CycleTransformation />
+      <CycleTransformation protocolId={protocolId} />
     </>
   );
 }
