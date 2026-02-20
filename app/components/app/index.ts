db448068e3d282455632462be45a4d5bbcc8f49c
Rudh <rudhkurup@conka.uk>
2026-02-20 18:01:18 +0000

    upgrading hero section into one component with new styling

--- app/components/app/index.ts
+++ app/components/app/index.ts
@@ -1,4 +1,2 @@
 export { default as AppHero } from "./AppHero";
-export { AppHeroDesktop } from "./AppHeroDesktop";
-export { AppHeroMobile } from "./AppHeroMobile";
 export type { AppHeroProps } from "./types";
