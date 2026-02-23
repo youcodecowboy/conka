016869a5bb267fd30d758779b25ca1272ca3cda4
Rudh <rudhkurup@conka.uk>
2026-02-23 14:18:55 +0000

    adding to index

--- app/components/app/index.ts
+++ app/components/app/index.ts
@@ -1,4 +1,5 @@
 export { default as AppHero } from "./AppHero";
 export { AppHeroDesktop } from "./AppHeroDesktop";
 export { AppHeroMobile } from "./AppHeroMobile";
+export { default as AppTestSection } from "./AppTestSection";
 export type { AppHeroProps } from "./types";
