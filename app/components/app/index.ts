1a914e0a9138d33b8b659b0e0117b81ac89c9582
Rudh <rudhkurup@conka.uk>
2026-02-23 18:37:02 +0000

    phone frame

--- app/components/app/index.ts
+++ app/components/app/index.ts
@@ -1,5 +1,5 @@
 export { default as AppHero } from "./AppHero";
 export { AppHeroDesktop } from "./AppHeroDesktop";
 export { AppHeroMobile } from "./AppHeroMobile";
-export { default as AppStickyPhoneBlock } from "./AppStickyPhoneBlock";
+export { default as AppStickyPhoneBlock, PhoneFrame } from "./AppStickyPhoneBlock";
 export type { AppHeroProps } from "./types";
